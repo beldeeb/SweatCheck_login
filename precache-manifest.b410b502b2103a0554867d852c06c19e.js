84469f8c44db453237a3bd23580a812127f6abdb
beldeeb <bassel_eldeeb@knights.ucf.edu>
2022-07-14 14:40:26 -0400

    Updates

---
+++ precache-manifest.b410b502b2103a0554867d852c06c19e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f436f8f2ee1378aaa6bf",
+    "url": "/SweatCheck_login/static/css/main.f90fd3cf.chunk.css"
+  },
+  {
+    "revision": "f436f8f2ee1378aaa6bf",
+    "url": "/SweatCheck_login/static/js/main.f436f8f2.chunk.js"
+  },
+  {
+    "revision": "22eccebd7614e18a0266",
+    "url": "/SweatCheck_login/static/css/1.3ee4559a.chunk.css"
+  },
+  {
+    "revision": "22eccebd7614e18a0266",
+    "url": "/SweatCheck_login/static/js/1.22eccebd.chunk.js"
+  },
+  {
+    "revision": "9b402bd959503f757e03",
+    "url": "/SweatCheck_login/static/js/runtime~main.9b402bd9.js"
+  },
+  {
+    "revision": "bbcc662a58e1aed83b10fbbdea98bb23",
+    "url": "/SweatCheck_login/static/media/logo.bbcc662a.png"
+  },
+  {
+    "revision": "faeb0f2555fa802ef85594c1ab0b5c66",
+    "url": "/SweatCheck_login/static/media/bg.faeb0f25.png"
+  },
+  {
+    "revision": "17ee156c1021cbcb45f07a2ab2864735",
+    "url": "/SweatCheck_login/index.html"
+  }
+];
